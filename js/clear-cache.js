d2cbaa0f1b4395d67673cbb85e47768f06a2d428
Rajkumar S <rajkumaar2304@icloud.com>
2020-07-09 00:28:43 +0530

    Update js/clear-cache.js

--- js/clear-cache.js
+++ js/clear-cache.js
@@ -1,6 +1,6 @@
 function clearCache(name){
 	localStorage.removeItem(name);
-	var node;node = document.getElementById("clear");
+	const node = document.getElementById("clear");
 	node.style.visibility = "hidden";
 	alert("Cache has been cleared");
 	window.location.reload(false);
